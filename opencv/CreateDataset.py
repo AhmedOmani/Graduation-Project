7cea23395d29d3e60df34420ef828d4958df5c1d
AhmedSaber <ahmedomani2003@gmail.com>
2024-12-14 16:04:13 +0200

    Create Model

--- opencv/CreateDataset.py
+++ opencv/CreateDataset.py
@@ -1,37 +1,39 @@
 import os
-import pickle
-
-import mediapipe as mp
 import cv2
-import matplotlib.pyplot as plt
 
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+number_of_classes = 3
+dataset_size = 100
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+cap = cv2.VideoCapture(0)
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
 
-DATA_DIR = './Data'
+    print('Collecting data for class {}'.format(j))
 
-data = []
-labels = []
-for dir in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir)):
-        img=cv2.imread(os.path.join(DATA_DIR, dir,img_path))
-        img_rgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        results = hands.process(img_rgb)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    img_rgb,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style()
-                )
-    
-    plt.figure()
-    plt.imshow(img_rgb)
+    done = False
+    while True:
+        ret, frame = cap.read()
+        frame=cv2.flip(frame,1)
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
 
-plt.show()
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        frame=cv2.flip(frame,1)
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
