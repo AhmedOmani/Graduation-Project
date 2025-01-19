7909d09f0cd8a59ffad14bfc85d7c29902b34dd4
AhmedSaber <109821372+AhmedOmani@users.noreply.github.com>
2025-01-19 15:14:21 +0200

    Update CreateDataset.py

--- opencv/CreateDataset.py
+++ opencv/CreateDataset.py
@@ -1,39 +1,50 @@
 import os
+import pickle
+
+import mediapipe as mp
 import cv2
+import matplotlib.pyplot as plt
+
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
-if not os.path.exists(DATA_DIR):
-    os.makedirs(DATA_DIR)
 
-number_of_classes = 3
-dataset_size = 100
+data = []
+labels = []
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
 
-cap = cv2.VideoCapture(0)
-for j in range(number_of_classes):
-    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
-        os.makedirs(os.path.join(DATA_DIR, str(j)))
+        x_ = []
+        y_ = []
 
-    print('Collecting data for class {}'.format(j))
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    done = False
-    while True:
-        ret, frame = cap.read()
-        frame=cv2.flip(frame,1)
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
-                    cv2.LINE_AA)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(25) == ord('q'):
-            break
+        results = hands.process(img_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
 
-    counter = 0
-    while counter < dataset_size:
-        ret, frame = cap.read()
-        frame=cv2.flip(frame,1)
-        cv2.imshow('frame', frame)
-        cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+                    x_.append(x)
+                    y_.append(y)
 
-        counter += 1
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
 
-cap.release()
-cv2.destroyAllWindows()
+            data.append(data_aux)
+            labels.append(dir_)
+
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
